a77c237bc7d97dd66448ee142fc3cd626ee06165
Juan <aichinojuani@gmail.com>
2025-01-28 19:54:45 -0300

    Se crea modulo para establecer conexion con base de datos y realizar consultas

---
+++ Modelo/database.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+class BaseDatos:
+
+    @staticmethod
+    def conexion():
+        try:
+            conn = sqlite3.connect('Market.db')
+            return conn
+        except Exception as e:
+            print(f'Error al conectarse con la base de datos - {e}')
+    
+
+    @staticmethod
+    def cerrar_conexion(conn):
+        if conn:
+            conn.close()
+    
+
+    @staticmethod
+    def realizar_consulta(sql, valores=None, tipo=None):
+        conn = BaseDatos.conexion()
+        if conn:
+            try:
+                cursor = conn.cursor()
+                if valores:
+                    cursor.execute(sql,valores)
+                else:
+                    cursor.execute(sql)
+
+                conn.commit()
+
+                if tipo == "SELECT":
+                    resultado = cursor.fetchall()
+                    return resultado
+            
+            except Exception as e:
+                print(f"Error al realizar consulta - {e}")
+                conn.rollback()
+            
+            finally:
+                cursor.close()
+                BaseDatos.cerrar_conexion(conn)
