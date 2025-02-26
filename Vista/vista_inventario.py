33d16c6fccacca0a5421cfc6c037aa6bdcc0d853
Juan Aichino <aichinojuani@gmail.com>
2025-02-26 10:45:44 -0300

    Se agregan comentarios y separadores para mejorar legibilidad

--- Vista/vista_inventario.py
+++ Vista/vista_inventario.py
@@ -3,7 +3,9 @@ from tkinter import ttk
 from vista.configuracion_vista import ConfiguracionVista as cv
 
 class VentanaInventario:
-
+    
+    # Constructor de la ventana
+    ###########################################################################
     def __init__(self, root):
         self.root = root
         self.root.title("Market - Inventario")
@@ -16,24 +18,25 @@ class VentanaInventario:
 
 
     def widgets(self):
-
+        
+        # Referencia a imagenes
+        #######################################################################
         self.img['filtrar'] = cv.formato_imagen(cv.imagenes['filtrar'])
         self.img['sinstock'] = cv.formato_imagen(cv.imagenes['sinstock'])
         self.img['astock'] = cv.formato_imagen(cv.imagenes['astock'])
         self.img['editar'] = cv.formato_imagen(cv.imagenes['editar'])
         self.img['eliminar'] = cv.formato_imagen(cv.imagenes['eliminar'])
-        
-        # Frame del Treeview
+
+        # Frames
+        #######################################################################
         self.frame_treeview = Frame(self.root, width=800, height=600)
         self.frame_treeview.place(x=1, y=0)
 
-        # Frame de funcionalidades
         self.frame_funcionalidades = Frame(
             self.root, width=400, height=600, bg=cv.colores['background']
         )
         self.frame_funcionalidades.place(x=800, y=0)
 
-        # Frame línea divisora funcionalidades
         self.frame_linea = Frame(
             self.frame_funcionalidades,
             width=400, 
@@ -43,7 +46,8 @@ class VentanaInventario:
         
         self.frame_linea.place(x=0, y=300)
 
-        # Titulo frame funcionalidades
+        # Labels
+        #######################################################################
         self.label_funcionalidades = Label(
             self.frame_funcionalidades,
             text="BÚSQUEDA",
@@ -54,7 +58,6 @@ class VentanaInventario:
             relx=0.5, rely=0.05, anchor='center'
         )
 
-        # Entry, Label y Button de búsqueda
         self.label_codigo = Label(
             self.frame_funcionalidades,
             text="Código", 
@@ -62,6 +65,7 @@ class VentanaInventario:
             bg=cv.colores['background']
         )
         self.label_codigo.place(relx=0.05, rely=0.12)
+        
         self.label_descripcion = Label(
             self.frame_funcionalidades,
             text="Descripción", 
@@ -70,18 +74,22 @@ class VentanaInventario:
         )
         self.label_descripcion.place(relx=0.05, rely=0.22)
 
+        # Entries
+        #######################################################################
         self.entry_codigo = Entry(
             self.frame_funcionalidades,
             font=cv.fuentes['texto'], 
             width=19
         )
-        
         self.entry_codigo.place(relx=0.4, rely=0.12)
+        
         self.entry_descripcion = Entry(
             self.frame_funcionalidades, font=cv.fuentes['texto'], width=19
         )
         self.entry_descripcion.place(relx=0.4, rely=0.22)
 
+        # Buttons
+        #######################################################################
         self.boton_buscar = Button(
             self.frame_funcionalidades, 
             text="Filtrar", 
@@ -104,7 +112,6 @@ class VentanaInventario:
         )
         self.boton_sin_stock.place(relx=0.5, rely=0.45, anchor='center')
 
-        # Botones nuevo producto, editar producto y eliminar producto
         self.boton_nuevo_producto = Button(
             self.frame_funcionalidades, 
             text="Nuevo Producto", 
@@ -115,6 +122,7 @@ class VentanaInventario:
             padx=10
         )
         self.boton_nuevo_producto.place(relx=0.5, rely=0.6,anchor='center')
+        
         self.boton_editar_producto = Button(
             self.frame_funcionalidades, 
             text="Editar Producto", 
@@ -127,6 +135,7 @@ class VentanaInventario:
         self.boton_editar_producto.place(
             relx=0.5, rely=0.75, anchor='center'
         )
+        
         self.boton_eliminar_producto = Button(
             self.frame_funcionalidades, 
             text="Eliminar Producto", 
@@ -140,7 +149,8 @@ class VentanaInventario:
             relx=0.5, rely=0.9, anchor='center'
         )
 
-        # Treeview inventario
+        # Treeview
+        #######################################################################
         self.treeview_inventario = ttk.Treeview(
             self.frame_treeview,
             columns=('col1','col2','col3'),
@@ -174,7 +184,8 @@ class VentanaInventario:
 
         self.treeview_inventario.grid(row=0, column=0, sticky='nswe')
 
-        # Scrollbar treeview
+        # Scrollbar
+        #######################################################################
         self.scrollbar = ttk.Scrollbar(
             self.frame_treeview,
             orient='vertical',
@@ -183,19 +194,23 @@ class VentanaInventario:
         self.treeview_inventario.config(yscrollcommand=self.scrollbar.set)
         self.scrollbar.grid(row=0, column=1, sticky='ns')
 
-
+    # Metodo limpieza de campos
+    ###########################################################################
     def limpiar_cajas(self):
         self.entry_descripcion.delete(0, 'end')
         self.entry_codigo.delete(0, 'end')
     
-
+    # Metodo limpieza Treeview
+    ###########################################################################
     def limpiar_treeview(self):
         for child in self.treeview_inventario.get_children():
             self.treeview_inventario.delete(child)
 
+
 class VentanaDetalleProducto:
 
-    # Constructor de la ventana Detalle de Producto
+    # Constructor de la ventana
+    ###########################################################################
     def __init__(self,root):
         self.root = root
         self.root.title("Inventario - Ventana de Producto")
@@ -203,14 +218,19 @@ class VentanaDetalleProducto:
             f"400x350+{int((cv.res_x-400)/2)}+{int((cv.res_y-350)/2)}"
         )
         self.root.resizable(0, 0)
+        self.img = {}
         self.root.config(bg=cv.colores['background'])
         self.widgets()
+
     
-    
-    # Widgets de la ventana
     def widgets(self):
         
-        # Label
+        # Referencia a imagenes
+        #######################################################################
+        self.img['guardar'] = cv.formato_imagen(cv.imagenes['guardar'])
+        
+        # Labels
+        #######################################################################
         self.label_descripcion = Label(
             self.root,
             text='Descripción',
@@ -235,7 +255,8 @@ class VentanaDetalleProducto:
         )
         self.label_cantidad.place(relx=0.05, rely=0.55)
 
-        # Entry
+        # Entries
+        #######################################################################
         self.entry_descripcion = Entry(
             self.root, font=cv.fuentes['texto'],width=20
         )
@@ -251,14 +272,23 @@ class VentanaDetalleProducto:
         )
         self.entry_cantidad.place(relx=0.35, rely=0.55)
 
-        # Button
+        # Buttons
+        #######################################################################
         self.boton_guardar = Button(
-            self.root, text='Guardar', font=cv.fuentes['boton'], width=15
+            self.root,
+            text='Guardar',
+            font=cv.fuentes['boton'],
+            width=150,
+            image=self.img['guardar'],
+            compound='left',
+            padx=10
         )
         self.boton_guardar.place(relx=0.5, rely=0.85, anchor='center')
-    
 
+    # Metodo limpieza de campos
+    ###########################################################################
     def limpiar_cajas(self):
         self.entry_cantidad.delete(0, 'end')
         self.entry_descripcion.delete(0, 'end')
         self.entry_precio.delete(0, 'end')
+        
